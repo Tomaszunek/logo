5506a43ceb0e3ed10002a3ecf321872eabe54a14
unknown <Tomaszunek>
2018-11-11 23:20:19 +0100

    add editable div package

--- src/components/commandEditor.tsx
+++ src/components/commandEditor.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
+import ContentEditable from 'react-contenteditable';
 
-export default class CommandEditor extends React.Component {
+export default class CommandEditor extends React.Component<IProps, IState> { 
+  constructor(props: any) {
+    super(props)
+    this.state = {html: "<b>Hello <i>World</i></b>"};
+  };
+
+  public handleChange = (evt:any) => {
+    this.setState({html: evt.target.value});
+  }
+
   public render() {
     return (
       <div className="commandEditor">
-        <div>
-          <div>abc</div>
-        </div>
+          <ContentEditable
+            html={this.state.html}
+            disabled={false}
+            onChange={this.handleChange}
+          />
       </div>
     );
-  }
+  }  
+}
+
+interface IProps {
+  text?: string | null
+}
+
+interface IState {
+  html: string
 }
 
