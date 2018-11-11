77e28087f819efde575fe5472f14f059c6b45779
unknown <Tomaszunek>
2018-11-11 11:07:27 +0100

    add scss to project, make simple UI

---
+++ src/components/commandList.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default class CommandList extends React.Component { 
+  
+  public render() {
+    return (
+      <div className="CommendList">
+        abc
+      </div>
+    );
+  }
+}
+
