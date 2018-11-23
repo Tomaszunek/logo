ab7487491bfe619259d79c19fef5ab29096d9990
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-11-23 23:40:09 +0100

    make 2 site structure for commands

--- src/components/helperWindow.tsx
+++ src/components/helperWindow.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { IPathwayExample } from 'src/models';
 import PathwayExample from './templates/pathwayExample';
+import CommandDescription from './templates/CommandDescription';
 
 export default class HelperWindow extends React.Component<IProps, IState> {  
   constructor(props: any) {
@@ -11,7 +12,7 @@ export default class HelperWindow extends React.Component<IProps, IState> {
   public render() {    
     return (
       <div style={this.props.itemStyle} className={"helperWindow " + this.props.site}>
-        {this.displayAll()}
+        {(this.props.site === 'right') ? (this.displayAll()) : (this.displayCommands())}
       </div>      
     );
   }
@@ -27,11 +28,9 @@ export default class HelperWindow extends React.Component<IProps, IState> {
           groups[item.type] = [item];
       }
     });
-    console.log(groups);
     const renderedTypes = [];
     for(const i in groups) {      
       if(groups[i]) {
-        console.log(groups[i]);
         renderedTypes.push(
           <div key={i}>
             <div className={"header " + i}>
@@ -53,11 +52,43 @@ export default class HelperWindow extends React.Component<IProps, IState> {
         <PathwayExample key={example.name} examplePath={example}/>
       )          
     })
+  }
+
+  private displayCommands() {
+    const { descriptions } = this.props;
+    const renderedTypesL = [];
+    const renderedTypesR = [];
+    let index = 0;
+    for(const i in descriptions) {      
+      if(descriptions[i]) {
+        if(index % 2 === 0) {
+          renderedTypesL.push(
+            <CommandDescription key={i} description={descriptions[i]}/>
+          )  
+        } else {
+          renderedTypesR.push(
+            <CommandDescription key={i} description={descriptions[i]}/>
+          ) 
+        }
+         index++;     
+      }
+    }
+    return (
+      <div className="commandsList">
+        <div className="commandsLeft">
+          {renderedTypesL}
+        </div>
+        <div className="commandsRight">
+          {renderedTypesR}
+        </div>
+      </div>
+    );
   }  
 }
 
 interface IProps {
   examplePaths: Array<IPathwayExample>,
+  descriptions: any,
   itemStyle: React.CSSProperties,
   site: string
 }
