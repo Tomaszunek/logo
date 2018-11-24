b9a0f5087d4b242fe77bb0db9fb7fac46cf58dad
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-11-24 23:50:09 +0100

    display desc

--- src/components/templates/CommandDescription.tsx
+++ src/components/templates/CommandDescription.tsx
@@ -9,18 +9,21 @@ export default class CommandDescription extends React.Component<IProps, IState> 
 
   public render() {
     // const { short, name, long,  color, description, argCount, args } = this.props.description;
-    const { name, color, short, long, description, argCount} = this.props.description;   
+    const { description } = this.props;   
  
     return (
       <div className="commandItem">
         <img src={commandImage}/>
         <div className="description">
-          {name}
-          {color}
-          {short}
-          {long}
-          {argCount}
-          {description}
+          {description.map((desc: any, index: string) => {
+            if(Array.isArray(desc)) {
+              return;
+            } else {
+              return <div key={index}>
+              {index + " : " + desc}
+            </div>
+            }            
+          })}
           {/* {args} */}
         </div>        
       </div>
