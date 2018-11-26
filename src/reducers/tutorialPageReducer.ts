d5f96d84157347513927fc7c560dda14b0cf829e
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-11-26 23:44:48 +0100

    make tutorial popup

--- src/reducers/tutorialPageReducer.ts
+++ src/reducers/tutorialPageReducer.ts
@@ -2,8 +2,7 @@ import { handleActions } from 'redux-actions';
 import { RootState } from './state';
 import { ITutorialPage } from '../models';
 
-const initialState: RootState.TutorialPages = [
-    
+const initialState: RootState.TutorialPages = [    
 ];
 
 export const tutorialPageReducer = handleActions<RootState.TutorialPages, ITutorialPage>(
