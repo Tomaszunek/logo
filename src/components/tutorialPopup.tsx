226283fa9b777ed17162d514c5ea60bf6f943f9e
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-11-27 18:45:29 +0100

    make tutorial functionality

--- src/components/tutorialPopup.tsx
+++ src/components/tutorialPopup.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
+import { ITutorialPage } from '../models'
 
 export default class TutorialPopup extends React.Component<IProps, IState> { 
   constructor(props: IProps) {
     super(props);
+    this.state = {
+      sideNumber: 0
+    }
   };  
 
   public render() {
     console.log(this.setVisibility())
+    const tutorialPage = this.props.tutorialPages[this.state.sideNumber]
     return (
       <div className="tutorialPopup">
-        <div>
-          turorial          
-        </div>
+        {this.displayContent(tutorialPage)}
         <div className="tutorialNav">
           <button>{"< BACK"}</button>
           <button>{"NEXT >"}</button>
@@ -23,13 +26,27 @@ export default class TutorialPopup extends React.Component<IProps, IState> {
   private setVisibility = () => {
     return 0;
   }
+
+  private displayContent = (tutorialPage: ITutorialPage) => {
+    if(tutorialPage) {
+      const { title } = tutorialPage;
+      return (
+        <div>
+            <button>X</button>
+            <p>{title}</p>      
+          </div>
+      ) 
+    } else {
+      return null;
+    }       
+  }
 }
 
 interface IProps {
-    tutorialPages: Array<any>,
+    tutorialPages: Array<ITutorialPage>,
 }
 interface IState {
-    showPopup: boolean
+    sideNumber: number
 }
   
   
